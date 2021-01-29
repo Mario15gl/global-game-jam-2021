6722c656e82233d4022f54c10b455f652d9afc12
Mario15gl <57710828+Mario15gl@users.noreply.github.com>
2021-01-29 21:04:46 +0000

    Basic Menu

---
+++ GlobalGameJam2021/Assets/Scripts/menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        //go to game scene
+    }
+
+    public void Controls()
+    {
+        anim.Play("Controls");
+    }
+
+    public void ControlsBack()
+    {
+        anim.Play("Controls_Back");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
