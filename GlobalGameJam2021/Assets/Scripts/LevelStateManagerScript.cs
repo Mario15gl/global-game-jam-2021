eda076a7e470e23be2ad899054e0a2f4c5692cb5
JordanGlendinningAbertay <1802879@uad.ac.ul>
2021-01-29 22:33:02 +0000

    Implemented UnityEvents into PlayerGoal
    - Allows multiple functions to be called when the player reaches the goal (Functions can be added via Inspector)

---
+++ GlobalGameJam2021/Assets/Scripts/LevelStateManagerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStateManagerScript : MonoBehaviour
+{
+    // Create state enums
+    private enum LevelState
+    {
+        INTRO,          // Introduction state
+        GAMEPLAY,       // Gameplay state
+        WIN,            // Win state
+        LOSS            // Lose state
+    }
+
+    // Variable containing the current level state
+    private LevelState currentState { get; set; }
+
+    // Set the LevelState to "WIN"
+    public void SetPlayerWin()
+    {
+        currentState = LevelState.WIN;  // Update state
+        Debug.Log("Game WON");          // Print Debug Message
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialise manager variables
+        currentState = LevelState.GAMEPLAY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update paths based on current state enum
+        switch (currentState)
+        {
+        case LevelState.INTRO:
+            // INTRO update
+            break;
+
+        case LevelState.GAMEPLAY:
+            // GAMEPLAY update
+            break;
+
+        case LevelState.WIN:
+            // WIN update
+            break;
+
+        case LevelState.LOSS:
+            // LOSS update
+            break;
+        }
+    }
+}
