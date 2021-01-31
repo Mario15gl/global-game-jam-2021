3b8bf3090c62a1dd964dd1cad72fd761a3cef881
CalumRonney <60162999+CalumRonney@users.noreply.github.com>
2021-01-31 13:34:14 +0000

    Sock Movement Mostly fixed
    Can lose jump when colliding with multiple things? also can jump up things

--- GlobalGameJam2021/Assets/Scripts/PlayerMovementScripts/PlayerMovement.cs
+++ GlobalGameJam2021/Assets/Scripts/PlayerMovementScripts/PlayerMovement.cs
@@ -14,25 +14,33 @@ public class PlayerMovement : MonoBehaviour
     
     void HandleInput()
     {
+        //axis controls for front and back movement + left right
         float FrontBack = Input.GetAxisRaw("FrontBackForce");
         float LeftRight = Input.GetAxisRaw("LeftRightForce");
+        //direction with combination of frontback and leftright movement
         Vector3 direction = new Vector3(LeftRight, 0, FrontBack);
         Vector3 forcePosition = new Vector3(0.0f, -0.5f, 0.0f);
 
+        //if direction is larger than deadzone
         if (direction.magnitude >= 0.1f)
         {
+            //find target angle for movement using direction vector and camera direction
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
 
-
+            //get final move direction using target angle and forward vector
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
+            //add force to player along movedirection with defined speed
             GetComponent<Rigidbody>().AddForceAtPosition(moveDirection * speed, transform.position );
         }
 
+        //if jump is pressed
         if(Input.GetAxisRaw("JumpPlayer") > 0 )
         {
+            //if player isn't already jumpping
             if (isJump == false )
             {
+                //add jump force and set player to jumpping
                 GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * jumpValue, transform.position);
                 isJump = true;
             }
@@ -50,9 +58,10 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    
+    private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKey(KeyCode.Space) == false && other.gameObject.tag != ("Player"))
+        if (Input.GetKey(KeyCode.Space) == false && other.gameObject.tag != ("Player") && isJump == true)
         {
             isJump = false;
         }
