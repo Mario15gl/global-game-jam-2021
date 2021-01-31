b18ae8620cfc7476b5abba872b30c36840190584
JordanGlendinningAbertay <1802879@uad.ac.ul>
2021-01-31 12:01:17 +0000

    Updated LevelStateManager
    - More inspector values

--- GlobalGameJam2021/Assets/Scripts/LevelStateManagerScript.cs
+++ GlobalGameJam2021/Assets/Scripts/LevelStateManagerScript.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelStateManagerScript : MonoBehaviour
 {
@@ -20,6 +22,17 @@ public class LevelStateManagerScript : MonoBehaviour
     // Collection of Player Spawn points
     public GameObject[] PlayerSpawnPoints;
 
+    // Reference to the Game-End text
+    public Text GameEndText;
+
+    // GameEndText scale rate
+    public float GameEndTextScaleRate;
+
+    // GaneEndText scale value
+    private Vector3 _gameEndTextScale;
+
+    private float _gameOverTimeElapsed = 0;
+
     // Level State enum
     LevelState currentState;
 
@@ -27,6 +40,9 @@ public class LevelStateManagerScript : MonoBehaviour
     public void SetPlayerWin()
     {
         currentState = LevelState.WIN;  // Update state
+        GameEndText.text = "You Win!";
+        PlayerMovement plyMovementComponent = PlayerObject.GetComponent<PlayerMovement>();
+        plyMovementComponent.isEnabled = false;
         Debug.Log("Game WON");          // Print Debug Message
     }
 
@@ -34,6 +50,9 @@ public class LevelStateManagerScript : MonoBehaviour
     public void SetPlayerLoose()
     {
         currentState = LevelState.LOSS;  // Update state
+        GameEndText.text = "You Loose!";
+        PlayerMovement plyMovementComponent = PlayerObject.GetComponent<PlayerMovement>();
+        plyMovementComponent.isEnabled = false;
         Debug.Log("Game LOST");          // Print Debug Message
     }
 
@@ -45,5 +64,35 @@ public class LevelStateManagerScript : MonoBehaviour
 
         // Spawn Player at one of the spawn points
         PlayerObject.transform.position = PlayerSpawnPoints[Random.Range(0, PlayerSpawnPoints.Length)].transform.position;
+
+        // Update the game end text
+        _gameEndTextScale = new Vector3(0.0f, 0.0f, 0.0f);
+        GameEndText.transform.localScale = _gameEndTextScale;
+    }
+
+    // stateManager update function
+    void Update()
+    {
+        // Display the game-end text on the players UI
+        if (currentState == LevelState.WIN || currentState == LevelState.LOSS) // If the game has ended...
+        {
+            _gameOverTimeElapsed += Time.deltaTime;
+
+            // Check to see if the UI text has finished scaling over-time
+            if (_gameEndTextScale.x < 1.0f) // If the scaling has finihsed...
+            {
+                // Increment scale
+                _gameEndTextScale.x += GameEndTextScaleRate * Time.deltaTime;
+                _gameEndTextScale.y += GameEndTextScaleRate * Time.deltaTime;
+                _gameEndTextScale.z += GameEndTextScaleRate * Time.deltaTime;
+                GameEndText.transform.localScale = _gameEndTextScale;
+            } 
+
+            // Time elapsed check
+            if(_gameOverTimeElapsed >= 5.0f) // If the time passed since the game has finished surpassed 5 seconds...
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
     }
 }
